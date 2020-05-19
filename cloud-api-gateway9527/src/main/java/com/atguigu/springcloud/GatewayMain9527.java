f2a27ece8dfa05eefcdaa051c58ec36c3e958445
zhangxiaolin <915719553@qq.com>
2020-05-19 23:26:27 +0800

    hystrix 监控开发、gateway网关开发

---
+++ cloud-api-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zhangxiaolin
+ * @version V1.0
+ * @Title: null.java
+ * @Package com.atguigu.springcloud
+ * @Description: TODO
+ * @date 2020/5/19 22:54
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zhangxiaolin
+ * @version V1.0
+ * @Title: null.java
+ * @Package com.atguigu.springcloud
+ * @Description: TODO
+ * @date 2020/5/19 21:03
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
